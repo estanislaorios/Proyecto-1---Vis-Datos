a948747f6aad4153b606fa5c2228f47ff74143b5
estanislaorios <erioszgaib@mail.utdt.edu>
2024-04-02 16:33:34 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import * as d3 from "d3"
+  import CarRows from "./lib/CarRows.svelte"
+
+  let numbers = [5, 18, 29, 75, 100]
+</script>
+
+<main>
+  <h1 class="headline">Porcentaje de combustible disponible</h1>
+  <div class="rectangulo"></div>
+  <!-- Canal: altura. Tipo: col con divs -->
+  <CarRows numbers={numbers} />
+  
+</main>
+
+<style>
+  .headline {
+    text-align: center;
+    text-decoration: underline;
+    background-color: rgb(99, 99, 99);
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  }
+  .rectangulo{
+    width: 2000%;
+    height: 150px;
+    background-color: rgb(99, 99, 99);
+    margin: 0%;
+  }
+</style>
---
+++ src/lib/CarRows.svelte
@@ -0,0 +1,47 @@
+<script>
+  import * as d3 from "d3"
+
+  export let numbers = []
+
+</script>
+ 
+<div class="car-container">
+  {#each numbers as n}
+  <div>
+    <div class="row-wrapper">
+      <div class="row-car" style="width: {n}px;  background-color: green;"></div>
+      <img class="car" src="./images/autocompleto.png" alt="">
+    </div>
+    <p class="number">{n}%</p>
+  </div>
+  {/each}
+</div>
+
+<style>
+  .car-container {
+    display: flex;
+    margin-top: -150px;
+    justify-content: center;
+  }
+  .row-wrapper {
+    position: relative;
+    width: 200px;
+    height: 120px;
+  }
+  .row-car {
+    position: absolute;
+    bottom: 20px;
+    left: 28px;
+    height: 60px;
+  }
+  .car {
+    position: absolute;
+    z-index: 1;
+    bottom: 0;
+  }
+  .number {
+    position: absolute;
+    text-align: center;
+    font-family: 'Roboto', sans-serif;
+  }
+</style>
